66193c2faa42f4dc18ad1102afd899b9bb542dd3
vinandius <157501650+vinandius@users.noreply.github.com>
2026-09-13 22:49:47 +0700

    Update NIM.js

--- Tugas2/NIM.js
+++ Tugas2/NIM.js
@@ -1,9 +1,18 @@
 const button1 = document.getElementById('b1');
+
+let isDeleted = false;
+
 button1.addEventListener('click', function() {
-    document.getElementById('p1').style.visibility = 'visible';
+    
+    if (isDeleted == true) {
+        alert("ID telah dihapus!");
+    } else {
+        document.getElementById('p1').style.visibility = 'visible';
+    }
 })
 
 const button2 = document.getElementById('b2');
 button2.addEventListener('click', function() {
-    document.getElementById('p1').style.visibility = 'hidden';
+    document.getElementById('p1').remove();
+    isDeleted = true;
 })
